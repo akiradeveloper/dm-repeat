89264d35e949498a0d1db9fade80f8204f4f251f
Akira Hayakawa <ruby.wktk@gmail.com>
2011-12-24 11:11:42 +0900

    change to dm-repeat.c

--- src/dm-repeat.c
+++ src/dm-repeat.c
@@ -8,18 +8,119 @@
  * Copyright (C) 2011 Akira Hayakawa <ruby.wktk@gmail.com>
  */
 
-#include <string.h>
 
 #include <linux/device-mapper.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/bio.h>
+#include <linux/string.h>
 
 #define DM_MSG_PREFIX "repeat"
 
-static char *str_rep = "akiradeveloper"; /* default */
-module_param(str_rep, charp, S_IRUGO);
-MODULE_PARM_DESC(str_rep, "string to repeat");
+static char *repseq = "0xff"; /* default */
+module_param(repseq, charp, S_IRUGO);
+MODULE_PARM_DESC(repseq, "byte sequence to repeat");
+
+struct byteseq {
+	size_t size;
+	void *src;
+};
+
+void free_byteseq(struct byteseq *p)
+{
+	kfree(p->src);
+	kfree(p);
+}
+
+int startw(char *s, char *prefix)
+{
+	int n = strlen(prefix);
+	char buf[n+1]; buf[n] = '\0';
+	memcpy(buf, s, n);
+	int r = strcmp(prefix, buf);
+	return r == 0;
+}
+
+struct byteseq *parse_arg(char *s)
+{
+	struct byteseq *result = kmalloc(sizeof(*result), GFP_KERNEL);
+	size_t sz = (strlen(s) - 2) / 2;
+	void *src = kmalloc(sz, GFP_KERNEL);
+	result->src = src;
+	result->size = sz;
+
+	int r = startw(s, "0x"); BUG_ON( !r );
+
+	char buf[3]; buf[2] = '\0';
+	int i, j;
+	unsigned int dx;
+	char cx;
+	for(i=0; i<sz; ++i){
+		j = i*2 + 2; // skip 0x prefix and get each 2 cs.
+		memcpy(buf, s+j, 2);
+		sscanf(buf, "%x", &dx);
+		cx = dx;
+		memcpy(src+i, &cx, 1);			
+	}		
+	return result;
+}
+
+void repeat_fill_mem(void *dest, size_t destlen, void *src, size_t srclen, size_t offset)
+{
+	int i;
+	int step = srclen - offset;
+	for(i=0; i<destlen; ){
+		memcpy(dest+i, src+offset, step);
+		i += step;
+		offset = 0;
+		if(i+srclen >= destlen){
+			step = destlen - i;
+		} else {
+			step = srclen;
+		}
+	}  
+}
+
+void repeat_fill_bio(struct bio *bio, char *s)
+{
+	unsigned long flags;
+	struct bio_vec *bv;
+	int i;
+
+	bio_for_each_segment(bv, bio, i) {
+		char *data = bvec_kmap_irq(bv, &flags);
+
+		struct byteseq *result = parse_arg(repseq);
+		int offset = 0;
+		repeat_fill_mem(data, bv->bv_len, result->src, result->size, offset);
+		free_byteseq(result);
+
+		flush_dcache_page(bv->bv_page);
+		bvec_kunmap_irq(data, &flags);
+		offset = (offset + bv->bv_len) % len;
+	}
+}
+
+static int repeat_map(struct dm_target *ti, struct bio *bio,
+		      union map_info *map_context)
+{
+	switch(bio_rw(bio)) {
+	case READ:
+		repeat_fill_bio(bio, repseq);
+		break;
+	case READA:
+		/* readahead of null bytes only wastes buffer cache */
+		return -EIO;
+	case WRITE:
+		/* writes get silently dropped */
+		break;
+	}
+
+	bio_endio(bio, 0);
+
+	/* accepted bio, don't make new request */
+	return DM_MAPIO_SUBMITTED;
+}
 
 static int repeat_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 {
@@ -34,61 +135,6 @@ static int repeat_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 	ti->num_discard_requests = 1;
 
 	return 0;
-}
-
-void repeat_fill_mem(void *data, size_t len_data, char *s, size_t offset)
-{
-	int i;
-	int sl = strlen(s);
-	int step = sl - offset;
-	for(i=0; i<len_data; ){
-		memcpy(data+i, s+offset, step);
-		i+=step;
-		offset = 0;
-		step = sl;
-		if(unlikely(i+step >= len_data)){
-			step = len_data - i;
-		}
-	}  
-}
-
-void repeat_fill_bio(struct bio *bio, char *s)
-{
-	unsigned long flags;
-	struct bio_vec *bv;
-	int i;
-
-	int len = strlen(s);
-	int offset = (bio->sector << 9) % len;
-
-	bio_for_each_segment(bv, bio, i) {
-		char *data = bvec_kmap_irq(bv, &flags);
-		repeat_fill_mem(data, bv->bv_len, s, offset);
-		flush_dcache_page(bv->bv_page);
-		bvec_kunmap_irq(data, &flags);
-		offset = (offset + bv->bv_len) % len;
-	}
-}
-
-static int repeat_map(struct dm_target *ti, struct bio *bio,
-		      union map_info *map_context)
-{
-	switch(bio_rw(bio)) {
-	case READ:
-		repeat_fill_bio(bio, str_rep);
-		break;
-	case READA:
-		/* readahead of null bytes only wastes buffer cache */
-		return -EIO;
-	case WRITE:
-		/* writes get silently dropped */
-		break;
-	}
-
-	bio_endio(bio, 0);
-
-	/* accepted bio, don't make new request */
-	return DM_MAPIO_SUBMITTED;
 }
 
 static struct target_type repeat_target = {
@@ -118,5 +164,5 @@ module_init(dm_repeat_init)
 module_exit(dm_repeat_exit)
 
 MODULE_AUTHOR("Akira Hayakawa");
-MODULE_DESCRIPTION(DM_NAME " dummy target repeating given string");
+MODULE_DESCRIPTION(DM_NAME " dummy target repeating given byte sequence");
 MODULE_LICENSE("GPL");
